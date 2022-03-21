559e961cc17d90f1a04d8e836df6ae1ad4485370
gwenversteegh <70900763+gwenversteegh@users.noreply.github.com>
2022-03-21 13:56:57 +0100

    werkt v1

---
+++ public/carousel.js
@@ -0,0 +1,84 @@
+
+
+
+
+// const carouselSlide = document.querySelector('main section ul:first-of-type');
+// const carouselQuotes = document.querySelectorAll('main section ul:first-of-type li');
+
+// //buttons
+// const ButtonPrev = document.querySelector('main button:first-of-type');
+// const ButtonNext = document.querySelector('main button:nth-of-type(2)');
+
+// //counter
+// let counter = 1;
+
+// //button listener
+
+// ButtonNext.addEventListener('click', () => {
+// carouselSlide.style.transition = "transform 0.4s ease-in-out";
+// counter++;
+// carouselSlide.style.transform = 'translateX(-' + (counter) + ')';
+
+// console.log(carouselQuotes);
+// console.log(counter);
+// });
+
+
+
+// var showQuote = document.querySelector("section:first-of-type > li");
+
+// var klikQuoteNrEen = document.querySelector("section:first-of-type ul:nth-of-type(2) li:first-of-type button");
+// var klikQuoteNrTwee = document.querySelector("section:first-of-type ul:nth-of-type(2) li:nth-of-type(2) button");
+// var klikQuoteNrDrie = document.querySelector("section:first-of-type ul:nth-of-type(2) li:nth-of-type(3) button");
+// var klikQuoteNrVier = document.querySelector("section:first-of-type ul:nth-of-type(2) li:nth-of-type(4) button");
+
+// function veranderQuoteInNrEen () {
+//     showQuote = document.querySelector('section:first-of-type ul:first-of-type li:first-of-type');
+     
+//      klikQuoteNrEen.classList.remove("active");
+//      klikQuoteNrTwee.classList.remove("active");
+//      klikQuoteNrDrie.classList.remove("active");
+//      klikQuoteNrVier.classList.remove("active");
+     
+//      klikQuoteNrEen.classList.add("active");
+//  }
+
+//  function veranderQuoteInNrTwee () {
+//     showQuote = document.querySelector('section:first-of-type ul:first-of-type li:nth-of-type(2)') ;
+    
+//     klikQuoteNrEen.classList.remove("active");
+//     klikQuoteNrTwee.classList.remove("active");
+//     klikQuoteNrDrie.classList.remove("active");
+//     klikQuoteNrVier.classList.remove("active");
+    
+//     klikQuoteNrTwee.classList.add("active");
+// }
+
+// function veranderQuoteInNrDrie () {
+//     showQuote = document.querySelector('section:first-of-type ul:first-of-type li:nth-of-type(3)') ;
+    
+//     klikQuoteNrEen.classList.remove("active");
+//     klikQuoteNrTwee.classList.remove("active");
+//     klikQuoteNrDrie.classList.remove("active");
+//     klikQuoteNrVier.classList.remove("active");
+    
+//     klikQuoteNrDrie.classList.add("active")
+// }
+
+// function veranderQuoteInNrVier () {
+//     showQuote = document.querySelector('section:first-of-type ul:first-of-type li:nth-of-type(4)') ;
+    
+//     klikQuoteNrEen.classList.remove("active");
+//     klikQuoteNrTwee.classList.remove("active");
+//     klikQuoteNrDrie.classList.remove("active");
+//     klikQuoteNrVier.classList.remove("active");
+    
+//     klikQuoteNrVier.classList.add("active")
+    
+// }
+
+//  klikQuoteNrEen.addEventListener('click', veranderQuoteInNrEen);
+//  klikQuoteNrTwee.addEventListener('click', veranderQuoteInNrTwee);
+//  klikQuoteNrDrie.addEventListener('click', veranderQuoteInNrDrie);
+//  klikQuoteNrVier.addEventListener('click', veranderQuoteInNrVier);
+
---
+++ public/modules/API.js
@@ -0,0 +1,66 @@
+
+// import quotes from API 
+const endpoint = 'https://quote.api.fdnd.nl/v1/quote'
+export const list = document.querySelector('ul:first-of-type')
+
+import { showLoadingState, hideLoadingState, showErrorState } from './states.js';
+
+
+showLoadingState ()
+getData()
+
+
+function getData() {
+    fetch(endpoint).then(function(response) {
+        return response.json()
+})
+    .then (quotes => {
+        hideLoadingState ()
+        quotes.data = quotes.data.slice(0,-5)
+        renderData (quotes)
+})
+    .catch((error) => {  
+        console.log ("het gaat niet goed...")
+        hideLoadingState()
+        showErrorState(error)
+});
+
+}
+
+let id = 0;
+
+function renderData(quotes){
+    quotes.data.forEach(data => {
+    list.insertAdjacentHTML('beforeend', 
+    `<li id=${id = id + 1}>
+        <img src='${data.avatar}' alt='profielfoto' onerror="this.src='./img/vervanging.jpg'" >
+        <h2>${data.name}</h2>
+        <p>${data.text}</p>
+    </li>`)
+ })
+}
+;
+
+// Als er meer komen, data lengte lezen (data.length)
+const quotes = [
+    '#1', '#2', '#3', '#4', '#5', '#6', '#7', '#8', '#9', '#10', '#11',
+]
+
+button.onclick = function() {
+    const randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
+
+    var base_url = window.location.origin;
+
+    window.location.href = base_url + "/citas-para-ti/eindopdracht/" +  randomQuote
+}
+
+const quoterandom = document.querySelector("#quoterandom")
+function randomQuotes(quotes) {
+    quoterandom.insertAdjacentHTML('beforeend', 
+    `<li id=${id = 2}>
+        <img src='${quotes.avatar}' alt='profielfoto' onerror="this.style.display='none'" >
+        <h2>${quotes.name}</h2>
+        <p>${quotes.text}</p>
+    </li>`)
+}
+
