13744c02877a22b0304080ac52e5055f66c48421
gwenversteegh <70900763+gwenversteegh@users.noreply.github.com>
2022-03-22 13:17:20 +0100

    het werkt 1

--- app.js
+++ app.js
@@ -1,20 +1,60 @@
 
 const http = require('http');
-
 const express = require('express')
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args))
+
 const app = express()
-const port = 3000
 
-app.use(express.static(__dirname + '/public'));
+// Stel ejs in als template engine
+app.set('view engine', 'ejs')
+app.set('views', 'views')
 
+// Stel een static map in
+app.use(express.static('public'))
 
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+// Maak een route voor de index
+app.get('/', function (req, res) {
+  // res.send('Hello world!')
+  res.send('hello world')
 })
 
-app.get("/", renderPage)
+// Maak een route voor quotes
+app.get('/quotes', function (req, res) {
+  fetch('https://quote.api.fdnd.nl/v1/quote')
+  .then(function(response){
+    return response.json()
+  })
+  .then((jsonData) =>{
+    res.render('quotes', {
+      data: jsonData.data,
+    })
+  })
+})
 
-function renderPage(req, res) {
-  res.sendFile(__dirname + "/public/index.html");
+// Maak een route voor een enkele quote
+// app.get('/quote/:id', function (req, res) {
+//   fetchJson(`https://quote.api.fdnd.nl/v1/quote/${req.params.id}`).then(function (jsonData) {
+//     res.render('quote', {
+//       pageTitle: 'Dit is een enkele quote pagina',
+//       quote: jsonData[0],
+//     })
+//   })
+// })
+
+app.set('port', process.env.PORT || 8000)
+
+const server = app.listen(app.get('port'), function () {
+  console.log(`Application started on port: ${app.get('port')}`)
+})
+
+/**
+ * Wraps the fetch api and returns the response body parsed through json
+ * @param {*} url the api endpoint to address
+ * @returns the json response from the api endpoint
+ */
+async function fetchJson(url) {
+  return await fetch(url)
+    .then((response) => response.json())
+    .then((body) => body.data)
+    .catch((error) => error)
 }
--- public/modules/states.js
+++ public/modules/states.js
@@ -1,19 +1,19 @@
-import {list} from './API.js'
+// import {list} from './API.js'
 
-export function showLoadingState(){
-    list.insertAdjacentHTML('afterbegin', 
-        `<p>
-        Some quotes are on the way, just a moment!
-        </p>`)
-}
+// export function showLoadingState(){
+//     list.insertAdjacentHTML('afterbegin', 
+//         `<p>
+//         Some quotes are on the way, just a moment!
+//         </p>`)
+// }
 
-export function hideLoadingState(){
-    list.removeChild(list.firstElementChild);
-}
+// export function hideLoadingState(){
+//     list.removeChild(list.firstElementChild);
+// }
 
-export function showErrorState(){
-    list.insertAdjacentHTML('afterbegin', 
-        `<p>
-        I'm sorry, something went wrong... please reload the page
-        </p>`)
-}
+// export function showErrorState(){
+//     list.insertAdjacentHTML('afterbegin', 
+//         `<p>
+//         I'm sorry, something went wrong... please reload the page
+//         </p>`)
+// }
