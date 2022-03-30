cc85ef663a5ff4ef885a628dac06097e20e7e777
gwenversteegh <70900763+gwenversteegh@users.noreply.github.com>
2022-03-30 10:54:35 +0200

    service worker enzo

--- app.js
+++ app.js
@@ -7,52 +7,52 @@ const app = express()
 
 // Stel ejs in als template engine
 app.set('view engine', 'ejs')
-app.set('views', 'views')
+app.set('views', './views');
+app.set('port', process.env.PORT || 8000)
 
 // Stel een static map in
 app.use(express.static('public'))
 
 // Maak een route voor de index
-app.get('/', function (req, res) {
-  // res.send('Hello world!')
-  res.send('hello world')
-})
+app.get("/", renderPagina)
+app.get("/offline", renderOffline)
+
+
 
 // Maak een route voor quotes
-app.get('/quotes', function (req, res) {
-  fetch('https://quote.api.fdnd.nl/v1/quote')
+function renderPagina (req, res){
+  fetch(`https://quote.api.fdnd.nl/v1/quote/`)
   .then(function(response){
     return response.json()
   })
+  
   .then((jsonData) =>{
-    res.render('quotes', {
+    jsonData.data = jsonData.data.slice(0,-5)
+    res.render('index', {
       data: jsonData.data,
+      data2: jsonData.data,
+      pageTitle: "Quotes"
     })
   })
-})
+}
 
-// Maak een route voor een enkele quote
-// app.get('/quote/:id', function (req, res) {
-//   fetchJson(`https://quote.api.fdnd.nl/v1/quote/${req.params.id}`).then(function (jsonData) {
-//     res.render('quote', {
-//       pageTitle: 'Dit is een enkele quote pagina',
-//       quote: jsonData[0],
-//     })
-//   })
-// })
-
-app.set('port', process.env.PORT || 8000)
 
 const server = app.listen(app.get('port'), function () {
   console.log(`Application started on port: ${app.get('port')}`)
 })
 
+
+function renderOffline (req, res){
+res.render('offline', {
+pageTitle: "Offline"
+  })
+}
 /**
  * Wraps the fetch api and returns the response body parsed through json
  * @param {*} url the api endpoint to address
  * @returns the json response from the api endpoint
  */
-async function fetchJson(url) {
+ async function fetchJson(url) {
   return await fetch(url)
     .then((response) => response.json())
     .then((body) => body.data)
