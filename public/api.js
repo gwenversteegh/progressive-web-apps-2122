d7fe65aa8d5f21aba27508dc8a7133a1094b7d7e
gwenversteegh <70900763+gwenversteegh@users.noreply.github.com>
2022-04-08 11:49:19 +0200

    Update api.js

--- public/api.js
+++ public/api.js
@@ -2,16 +2,12 @@ const button = document.querySelector("#button")
 const quoterandom = document.querySelector("#quoterandom")
 
 const quotes = ['#1', '#2', '#3', '#4', '#5', '#6', '#7', '#8', '#9', '#10']
-  
 
-button.addEventListener('click', () => {
+function showRandomQuote() {
     const randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
     var base_url = 'http://localhost:8000/';
     
-
     window.location.href = base_url + randomQuote
-})
+}
 
-
-
-
+button.addEventListener('click', showRandomQuote)
