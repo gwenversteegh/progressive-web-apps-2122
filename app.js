b49884b160107a9cb8bf37c672767b2677d2b159
gwenversteegh <70900763+gwenversteegh@users.noreply.github.com>
2022-10-03 22:29:41 +0200

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use (minify());
 app.get("/", renderPagina)
 app.get("/offline", renderOffline)
 
-.get('/times', (req, res) => res.send(showTimes()))
+// .get('/times', (req, res) => res.send(showTimes()))
 
 
 
@@ -70,11 +70,11 @@ function renderOffline (req, res){
     .catch((error) => error)
 }
 
-showTimes = () => {
-  let result = '';
-  const times = process.env.TIMES || 5;
-  for (i = 0; i < times; i++) {
-    result += i + ' ';
-  }
-  return result;
-}
+// showTimes = () => {
+//   let result = '';
+//   const times = process.env.TIMES || 5;
+//   for (i = 0; i < times; i++) {
+//     result += i + ' ';
+//   }
+//   return result;
+// }
