98c656006271daca376c0e7d3307396fb87a1770
gwenversteegh <70900763+gwenversteegh@users.noreply.github.com>
2022-10-03 19:54:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ function renderPagina (req, res){
 }
 
 
-const server = app.listen(app.get('port'), function () {
+const server = app.listen(app.get('5000'), function () {
   console.log(`Application started on port: ${app.get('port')}`)
 })
 
